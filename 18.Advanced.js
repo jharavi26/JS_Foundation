58269c36c78f028daafe0ce07edd09e8ddb5eb9a
Ravikumar Vishwanath Jha <120722758+jharavi26@users.noreply.github.com>
2026-01-10 00:29:04 +0530

    Enhance 18.Advanced.js with new JavaScript features
    Added examples of nullish coalescing, logical OR, template literals, destructuring assignment, spread and rest operators, and debounce function.

--- 18.Advanced.js
+++ 18.Advanced.js
@@ -1,5 +1,68 @@
+// a ?? b → returns a unless a is null or undefined. If a is null or undefined, returns b.
+
+//export default for single export
+//export for multiple named exports
+
+console.log(false ?? 26);
+console.log(null ?? 26);
+console.log(undefined ?? 26);
+console.log("Ravi" ?? 26);
+
+// a || b → returns a unless a is falsy (false, 0, '', NaN, null, undefined). If a is falsy, returns b.
+
+console.log(false || 26);
+console.log(null || 26);
+console.log(undefined || 26);
+console.log("Ravi" || 26);
+
+//Template Literals
+let user = "ART";
+console.log(`${user}`)
+
+//Destructing Assignment 
+
+const [r, s, t = 31] = [11,22];
+console.log(r,s,t);
+
+const{name , age} = {name : "Ravi", age : 26};
+console.log(name);
+
+//Spread Operator : expands arrays/objects
+
+const arr = [21,31];
+const arr2 = [...arr, 51];
+console.log(arr2);
+
+//Rest Operator : collects Remaining Elements
+
+function result (a, ...num){
+  return num.reduce((acc, num)=>acc+num, a)
+}
+console.log(result(1,2,3,4,5));
+
 // Debouncing :- It ensures that a function is only called once after a specified period of time has passed since the last time it was invoked. 
 //If a new event is triggered before the delay ends, the previous call is canceled, and the timer restarts.
+
+//Debounce 
+function debounce(func , delay){
+  let timer ;
+  return function(...args){
+  const context = this;
+  clearTimeout(timer);
+
+  timer = setTimeout(() =>func.apply(context, args) , delay);
+}
+}
+
+function art (){
+  console.log("Art")
+}
+
+const debounceLog = debounce(art, 1000)
+
+debounceLog();
+debounceLog();
+debounceLog();
 
 let timer;
 document.getElementById("input").addEventListener("keydown", ()=>{
