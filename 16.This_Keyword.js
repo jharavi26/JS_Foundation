3267383c749c1d71bede97318236b87d11670634
Ravikumar Vishwanath Jha <120722758+jharavi26@users.noreply.github.com>
2026-01-10 00:34:35 +0530

    Refactor this keyword examples with polyfills

--- 16.This_Keyword.js
+++ 16.This_Keyword.js
@@ -18,49 +18,80 @@
 // Event Listener (Regular)	:- The target element
 // Event Listener (Arrow Function):- 	Lexical this
 
-
-
-function art()
-{
-  console.log(this);
-}
-art();
-
-console.log(this);
-
-//Call():- Invokes a function immediately with a specific this value and arguments provided one by one.
-// function.call(this.arguments, arg1, arg2, ...)
-
-function greet(greeting, punctuation) {
-  console.log(`${greeting}, ${this.name}${punctuation}`);
+const person = {
+  name : "Ravikumar",
+  lastName : "Jha" , 
 }
 
-const person = { name: "Alice" };
-
-greet.call(person, "Hello", "!"); // Output: Hello, Alice!
-
-//Apply():-Similar to call, but arguments are passed as an array (or array-like object).
-function greet(greeting, punctuation) {
-  console.log(`${greeting}, ${this.name}${punctuation}`);
+function fullname (age , city){
+  console.log(`Name is ${this.name} & lastName is ${this.lastName} & age is ${age} & live in ${city} `)
 }
 
-const person1 = { name: "Alice" };
+fullname.call(person , 27 , "Mumbai");
 
-greet.apply(person1, ["Hi", "?"]); // Output: Hi, Alice?
+// Call PolyFills 
+ 
+Function.prototype.myCall = function(context = {} , ...args){
+  if(typeof this !== "function"){
+    throw new Error(this + "It is not Callable");
+  }
 
-//Bind():-Returns a new function with a specific this value and optional pre-set arguments. It doesn't invoke the function immediately.
-function greet(greeting, punctuation) {
-  console.log(`${greeting}, ${this.name}${punctuation}`);
+  context.fn = this;
+  context.fn(...args);
+
 }
 
-const person3 = { name: "Art" };
+fullname.myCall(person, 26 , "Delhi")
 
-const greetAlice = greet.bind(person3, "Hey");
-greetAlice("!"); // Output: Hey, Alice!
+
+//Apply Polyfills
+
+Function.prototype.myApply = function(context = {}, args = []){
+  if(typeof this !== "function"){
+    throw new console.error(this + "It is not Callable");
+  }
+
+  if(!Array.isArray(args)){
+    throw new TypeError("CreateListFromArray like called on non-object");
+  }
+
+  context.fn = this;
+  context.fn(...args);
+
+}
+
+fullname.myApply(person, [22, "Surat"])
+
+//Bind polyfills 
+
+// Bind polyfill
+const person2 = {
+  name : "Ravikumar",
+  LastName : "Jha",
+}
+
+function greet(city){
+  console.log(`${this.name} & lastName is ${this.LastName} & live in ${city}`)
+}
+
+
+Function.prototype.myBind = function(context = {}, ...args){
+  if(typeof this !== "function"){
+    throw new Error("This is not Bounded")
+  }
+
+  context.fn = this;
+   return function(...newArgs){
+    context.fn(...args, ...newArgs)
+   };
+};
+
+const newFunc = greet.myBind(person2, "Mumbai");
+newFunc();
+
 
 
 //bind creates a new function and does not invoke it immediately. The new function can be called later.
-
 
 // UseCase :- 
 // call():- Borrowing methods from other objects.
